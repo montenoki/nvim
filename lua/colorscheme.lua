4e6347c2cfbb24ba0848ab2b2fd019ee0744bbb1
monten.oki <monten.oki@gmail.com>
2023-05-17 22:58:46 +0900

    save

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,15 +1,10 @@
 vim.o.background = 'dark'
 
--- local colorscheme = 'dracula'
-local colorscheme
-if lite_mode then
-    colorscheme = 'solarized8'
-else
-    colorscheme = 'dracula'
-end
-
-local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
-if not status_ok then
-    vim.notify('colorscheme: ' .. colorscheme .. ' Not Found!')
-    return
+if not lite_mode then
+    local colorscheme = 'dracula'
+    local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+    if not status_ok then
+        vim.notify('colorscheme: ' .. colorscheme .. ' Not Found!')
+        return
+    end
 end
