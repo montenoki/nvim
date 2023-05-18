95ed5c853bbaad4e08f45aed2bbd7d5345a4d7e7
monten.oki <monten.oki@gmail.com>
2023-05-18 15:23:44 +0900

    save

--- lua/plugin-config/appearance/todo-comments.lua
+++ lua/plugin-config/appearance/todo-comments.lua
@@ -1,23 +1,24 @@
 local todo = requirePlugin('todo-comments')
+local uConfig = require('uConfig')
+local lite_mode = uConfig.lite_mode
 if todo == nil then
     return
 end
 
 todo.setup({
     keywords = {
-        FIX = {
-            icon = ' ', -- icon used for the sign, and in search results
-            color = 'error', -- can be a hex color, or a named color (see below)
-            alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
-            -- signs = false, -- configure signs for some keywords individually
+        if lite_mode then
+            TODO = { icon = '? ', color = 'info' },
+            NOTE = { icon = '::', color = 'hint', alt = { 'INFO' } },
+            WARN = { icon = '! ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+            FIX = { icon = '! ', color = 'error', alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' } },
+        else
+            TODO = { icon = ' ', color = 'info' },
+            NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+            WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+            FIX = { icon = ' ', color = 'error', alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' } },
+        end
         },
-        TODO = { icon = ' ', color = 'info' },
-        HACK = { icon = ' ', color = 'warning' },
-        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-        TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
-    },
     search = {
         command = 'rg',
         args = {
