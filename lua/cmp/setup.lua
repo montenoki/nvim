515c7eca0c3c53e15dd0f5b874e32a66ee5d3a09
monten.oki <monten.oki@gmail.com>
2023-05-19 11:57:49 +0900

    save

--- lua/cmp/setup.lua
+++ lua/cmp/setup.lua
@@ -52,7 +52,7 @@ cmp.setup({
     sources = cmp.config.sources(
         {
             -- { name = 'nvim_lsp', group_index = 1 },
-            { name = 'ultisnips', group_index = 1 },
+            -- { name = 'ultisnips', group_index = 1 },
             { name = 'vim-snippets', group_index = 1 },
             -- { name = 'nvim_lsp_signature_help', group_index = 1 }
         },
