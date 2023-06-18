003141acb840e1688900564d7528ab53d1be2c08
monten.oki <monten.oki@gmail.com>
2023-06-19 01:24:05 +0900

    save

--- lua/lsp/config/pyright.lua
+++ lua/lsp/config/pyright.lua
@@ -1,40 +1,23 @@
-local function auto_setup_debugpy()
-
-    print("1")
-    local fn = vim.fn
-    local sep
-    local exe
-    if getSysName() == 'Windows_NT' or getSysName() == 'Windows' then
-        sep = '\\'
-        exe = 'python.exe'
+local common = require('lsp.common-config')
+local function check_debugpy_exsit()
+    local executable_path
+    local os_name = getSysName()
+    if os_name == 'Windows' or os_name == 'Windows_NT' then
+        executable_path = '\\.virtualenvs\\Scripts\\python.exe'
     else
-        sep = '/'
-        exe = 'bin/python'
+        executable_path = '/.virtualenvs/debugpy/bin/python'
     end
-    local dot_env_path = vim.env.HOME .. sep .. '.virtualenvs'
-    local debugpy_path = dot_env_path .. sep .. 'debugpy'
-
-    print('2')
-
-    print('auto setup func!')
-
-    if fn.empty(fn.glob(debugpy_path)) > 0 then
-        vim.notify('Installing debugpy... Please wait...')
-        fn.system({ 'mkdir', dot_env_path })
-        fn.system({ 'python', '-m', 'venv', debugpy_path })
-        fn.system({ debugpy_path .. sep .. exe, '-m', 'pip', 'install', 'debugpy' })
-    else
-        print("alge")
+    local path = vim.env.HOME .. executable_path
+    if vim.fn.empty(vim.fn.glob(path)) > 0 then
+        vim.notify("No Python adapter for Dap found.", 'warning')
     end
 end
-
-local common = require('lsp.common-config')
 local opts = {
     capabilities = common.capabilities,
     on_attach = function(client, bufnr)
+        check_debugpy_exsit()
         common.keyAttach(bufnr)
         common.BreadcrumbsAttach(client, bufnr)
-        auto_setup_debugpy()
     end,
 }
 return {
