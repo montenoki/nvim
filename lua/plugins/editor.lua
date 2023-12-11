52745c069f2e3761df8e28f4c2a21d129c6adbe9
monten.oki <monten.oki@gmail.com>
2023-12-11 13:12:49 +0900

    save

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,5 @@
-if true then return {} end
 return {
+  -- File Explorer
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -7,15 +7,21 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-  {
-    'nvim-tree/nvim-tree.lua',
-    requires = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      require('plugin-configs.nvim-tree')
-    end,
-    tag = 'nightly',
+      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      {"<A-m>", "<CMD>Neotree toggle<CR>"},
+    },
+    opts = function ()
+      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+      vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
+      vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
+      vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
+      vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
+      return {
+        close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+
+      }
+    end
   },
 }
