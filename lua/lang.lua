fc0f9b22f6803ba001053d1303b494d5e259a1a4
monten.oki <monten.oki@gmail.com>
2023-12-12 18:31:26 +0900

    lsp init

---
+++ lua/lang.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+-- mason
+-- Server List:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+M.mason_servers = {
+  'stylua',
+  'shfmt',
+  -- 'bashls',
+  -- 'lua_ls',
+  -- 'marksman',
+  -- 'rust_analyzer',
+  -- 'pylsp',
+  -- 'vimls',
+  -- 'r_language_server',
+  -- 'clangd',
+  -- 'jsonls',
+  -- 'taplo',
+}
+
+return M
