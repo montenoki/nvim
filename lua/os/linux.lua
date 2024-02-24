5a727800a93328c26ec77e105504e576c62dff4d
monten.oki <monten.oki@gmail.com>
2024-02-24 18:35:27 +0900

    fix im-select for linux

--- lua/os/linux.lua
+++ lua/os/linux.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+  -- Switch Input Method automatically depends on NeoVim's edit mode.
+  {
+    'keaising/im-select.nvim',
+    opts = {
+      keep_quiet_on_no_binary = true,
+    },
+  },
+}
