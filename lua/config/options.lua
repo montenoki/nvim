52cd5aa88ed26b68e892cecae32fb9cc6f0973d3
montenoki <61886293+montenoki@users.noreply.github.com>
2025-01-08 10:06:39 +0900

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -71,3 +71,7 @@ opt.wrap = true
 
 -- fix the problem that cant see last char when scrollbar on.
 -- opt.virtualedit = "onemore"
+
+-- Example for configuring Neovim to load user-installed installed Lua rocks:
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
