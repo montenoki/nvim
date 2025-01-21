397d5886b08ddf004db242a737cd3ed188700f00
monten.oki <monten.oki@gmail.com>
2025-01-21 15:57:11 +0900

    fix(deps): update plugins and fix lspconfig API usage
    - Update multiple plugin dependencies to their latest versions
    - Fix lspconfig API call from `util.available_servers()` to `util._available_servers()`

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -28,7 +28,7 @@ return {
             capabilities.textDocument.foldingRange =
                 { dynamicRegistration = false, lineFoldingOnly = true }
             local language_servers =
-                require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+                require("lspconfig").util._available_servers() -- or list servers manually like {'gopls', 'clangd'}
             for _, ls in ipairs(language_servers) do
                 require("lspconfig")[ls].setup({ capabilities = capabilities })
             end
