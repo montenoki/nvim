61651ff651864aa067d16007b1574e7566f19958
monten.oki <monten.oki@gmail.com>
2025-01-22 14:58:06 +0900

    update obsidian localsetting

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,3 +1,33 @@
+local default_config = {
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    -- workspaces = {
+    --     {
+    --         name = "default",
+    --         path = "~/vaults/default",
+    --     },
+    -- },
+}
+
+-- 尝试加载本地配置
+local function load_local_config()
+    local local_config_path = vim.fn.stdpath("config")
+        .. "/lua/local/obsidian.lua"
+    local ok, local_config = pcall(dofile, local_config_path)
+    if ok then
+        return local_config
+    else
+        -- 返回默认配置
+        return default_config
+    end
+end
+
 local M = {
     "epwalsh/obsidian.nvim",
     version = "*",
@@ -7,34 +37,13 @@ local M = {
         "nvim-lua/plenary.nvim",
     },
 }
+local config = load_local_config()
 
--- 尝试加载本地配置
-local function load_local_config()
-    local local_config_path = vim.fn.stdpath("config")
-        .. "/lua/local/settings.lua"
-    local ok, local_config = pcall(dofile, local_config_path)
-    if ok then
-        return local_config
-    else
-        -- 返回默认配置
-        return {
-            workspaces = {
-                {
-                    name = "default",
-                    path = "~/vaults/default",
-                },
-            },
-        }
-    end
+if config.event ~= nil then
+    M.event = config.event
 end
-
-function M.config()
-    local local_settings = load_local_config()
-
-    return {
-        workspaces = local_settings.workspaces,
-        -- 其他通用配置...
-    }
+if config.opt ~= nil then
+    M.opt = config.opt
 end
 
 return M
