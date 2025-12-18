6baa05c25afdaef94d01d79df72e93cf3e3bc806
monten.oki <monten.oki@gmail.com>
2025-12-18 20:31:59 +0900

    add claudecode

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,13 @@
+return {
+    "greggh/claude-code.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+        require("claude-code").setup({
+            window = {
+                position = "vertical",
+            },
+        })
+    end,
+}
