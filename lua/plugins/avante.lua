8e1cc57c7a0e8698a5cddaaa0462e8eefa4b9032
monten.oki <monten.oki@gmail.com>
2025-12-20 02:56:02 -0500

    update avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,33 +1,46 @@
 return {
     {
         "yetone/avante.nvim",
-        event = "VeryLazy",
-        lazy = false,
-        version = false, -- set this if you want to always pull the latest change
-        opts = {
-            provider = "claude",
-            -- auto_suggestions_provider = "copilot",
-            auto_suggestions_provider = "claude",
-            suggestion = {
-                debounce = 100,
-                throttle = 600,
-            },
-            behaviour = { auto_suggestions = false },
-        },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-        build = function()
-            return vim.fn.has("win32") == 1
-                    and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-                or "make"
-        end,
+        -- ⚠️ must add this setting! ! !
+        build = vim.fn.has("win32") ~= 0
+                and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+            or "make",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        ---@module 'avante'
+        ---@type avante.Config
+        opts = {
+            instructions_file = "avante.md",
+
+            provider = "openrouter",
+
+            providers = {
+                openrouter = {
+                    __inherited_from = "openai",
+                    api_key_name = "OPENROUTER_API_KEY",
+                    endpoint = "https://openrouter.ai/api/v1",
+                    model = "anthropic/claude-4.5-sonnet",
+                    timeout = 30000,
+                    extra_request_body = {
+                        temperature = 0.75,
+                        max_tokens = 20480,
+                    },
+                },
+            },
+        },
         dependencies = {
-            "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
             --- The below dependencies are optional,
-            -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+            "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua", -- for file_selector provider fzf
+            "stevearc/dressing.nvim", -- for input provider dressing
+            "folke/snacks.nvim", -- for input provider snacks
             "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
             {
                 -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
