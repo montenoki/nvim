9deede5974bda135e812bffdd6b9f7c0d32a969e
monten.oki <montenoki@users.noreply.github.com>
2026-08-28 11:19:58 +0900

    打开文件后自动关闭 Neo-tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -23,6 +23,14 @@ return {
     {
         "nvim-neo-tree/neo-tree.nvim",
         opts = {
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function()
+                        require("neo-tree.command").execute({ action = "close" })
+                    end,
+                },
+            },
             window = {
                 mappings = {
                     ["v"] = "vsplit_with_window_picker",
